f748c2829eb96b7711b7675db9503b0bcb179796
James Clarke <cla16054@byui.edu>
2019-01-18 14:00:56 -0700

    week 3 placeholder upload

---
+++ web/Week3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+   <head>
+       <meta name="viewport" content="width=device-width, initial-scale=1"> 
+<script type="text/javascript" src="assignments_js.js"></script>
+       <link rel="stylesheet" type="text/css" href="assignments_css.css">
+              <title>Week 3</title>
+    
+   </head>
+    <?php include "./navbar.php"; ?>
+   <body>
+   <p>Awaiting Construction</p>
+ 
+   </body>
+</html> 
