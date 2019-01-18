e8d174f89dc263a4b9dd907fc88241098513f1ea
James Clarke <cla16054@byui.edu>
2019-01-18 13:55:34 -0700

    week 2 assignments upload

---
+++ web/assignments.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+   <head>
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       <script type="text/javascript" src="assignments_js.js"></script>
+       <link rel="stylesheet" type="text/css" href="assignments_css.css">
+       <title>Write Turn</title>
+       
+   </head>
+    <?php include "./navbar.php"; ?>
+   <body>
+       <div id="title">
+           <h1>Write Turn</h1>
+       </div>
+        <div id="pen">
+        <img src="pens.jpg" alt="Pens I made." width="550" height="345">
+        </div>
+     <div id="text">
+       <p>
+           I really enjoy creating pens on my wood lathe.  I have been making them since 1994.  I have collected many different tools to make them and need a way to keep track of them all so I don't purchase duplicates.
+     </p>
+       </div>
+       <div id="links">
+         <ul>
+             <li class="store"><a href="https://www.woodturnerscatalog.com">Craft Supplies USA</a></li>
+             <li class="store"><a href="https://www.pennstateind.com">Penn State Industries</a></li>
+             <li class="store"><a href="https://www.woodcraft.com">Woodcraft</a></li>
+         </ul>
+       </div>
+   </body> 
+</html> 
