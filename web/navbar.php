65a33d16cbcacec9782febf988adb950a85a8760
James Clarke <cla16054@byui.edu>
2019-02-02 16:05:46 -0700

    upload tool page

--- web/navbar.php
+++ web/navbar.php
@@ -2,6 +2,7 @@
     <ul>
         <li class="home"><a class="active" href="assignments.php">Home</a></li>
         <li class="#week3"><a href="browseItems.php">Products</a></li>
+        <li class="#week5"><a href="toolData.php">Tools</a></li>
     </ul>
 </nav>
  <!-- adapted from https://www.w3schools.com/bootstrap/bootstrap_navbar.asp and https://www.sitepoint.com/community/t/how-to-call-a-menu-file-with-php/242421 --> 
