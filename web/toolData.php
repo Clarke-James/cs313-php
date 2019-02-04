df6ddc2b549ceb24a0eec326ab4da09be0104d0f
James Clarke <cla16054@byui.edu>
2019-02-04 15:14:31 -0700

    upload tool page

--- web/toolData.php
+++ web/toolData.php
@@ -2,10 +2,20 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="navBar.css">
     <title>Tool Database</title>
 </head>
 <body>
+    <form>
+        Part Name: <input type="text"><br>
+        Part Number: <input type="text"><br>
+        Manufacturer: <input type="text"><br>
+        Picture Name: <input type="text"><br>
+        Location Type: <input type="text"><br>
+        Location: <input type="text"><br>
+    </form>
+    
 <?php
 try
 {
@@ -22,6 +32,7 @@ $dbName = ltrim($dbOpts["path"],'/');
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Database connected";
 }
 catch (PDOException $ex)
 {
@@ -30,18 +41,19 @@ die();
 }
 
 echo "Database output";
-$stmt = $db->prepare('SELECT * FROM bushings WHERE bushingId=:bushing_id AND bushingName=:bushing_name');
-//AND partNumber=:part_number AND manufacturer=:manufacturer AND pictureName=:picture_name//
-$stmt->execute(array(':bushingName' => $bushingName, ':bushingId' => $bushingId));
-       // ':partNumber' => $partNumber, ':manufacturer' => $manufacturer,  ':pictureName' => $pictureName//));
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($db->query('SELECT bushing_id, bushing_name FROM bushings') as $row)
-{
-    echo 'Bushing ID: ' . $row['bushing_id'];
-    echo ' Bushing Name: ' . $row['bushing_name'];
-    echo '<br/>';
-}
+//$stmt = $db->prepare('SELECT * FROM bushings WHERE bushingId=:bushing_id AND bushingName=:bushing_name');
+//AND partNumber=:part_number AND manufacturer=:manufacturer AND pictureName=:picture_name//
+//$stmt->execute(array(':bushingName' => $bushingName, ':bushingId' => $bushingId));
+       // ':partNumber' => $partNumber, ':manufacturer' => $manufacturer,  ':pictureName' => $pictureName//));
+//$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//foreach ($db->query('SELECT bushing_id, bushing_name FROM bushings') as $row)
+//{
+ //   echo 'Bushing ID: ' . $row['bushing_id'];
+//    echo ' Bushing Name: ' . $row['bushing_name'];
+//    echo '<br/>';
+//}
 ?>
 
 
