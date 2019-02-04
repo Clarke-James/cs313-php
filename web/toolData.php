7102bcecaf311c6ff2b0147ae531e80202bec0c6
James Clarke <cla16054@byui.edu>
2019-02-04 15:17:16 -0700

    upload tool page

--- web/toolData.php
+++ web/toolData.php
@@ -32,7 +32,7 @@ $dbName = ltrim($dbOpts["path"],'/');
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Database connected";
+    echo "Database connected <br>";
 }
 catch (PDOException $ex)
 {
@@ -40,7 +40,7 @@ echo 'Error!: ' . $ex->getMessage();
 die();
 }
 
-echo "Database output";
+echo "Database output <br>";
 
 //$stmt = $db->prepare('SELECT * FROM bushings WHERE bushingId=:bushing_id AND bushingName=:bushing_name');
 //AND partNumber=:part_number AND manufacturer=:manufacturer AND pictureName=:picture_name//
@@ -48,12 +48,12 @@ echo "Database output";
        // ':partNumber' => $partNumber, ':manufacturer' => $manufacturer,  ':pictureName' => $pictureName//));
 //$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-//foreach ($db->query('SELECT bushing_id, bushing_name FROM bushings') as $row)
-//{
- //   echo 'Bushing ID: ' . $row['bushing_id'];
-//    echo ' Bushing Name: ' . $row['bushing_name'];
-//    echo '<br/>';
-//}
+foreach ($db->query('SELECT bushing_id, bushing_name FROM bushings') as $row)
+{
+    echo 'Bushing ID: ' . $row['bushing_id'];
+    echo ' Bushing Name: ' . $row['bushing_name'];
+    echo '<br/>';
+}
 ?>
 
 
