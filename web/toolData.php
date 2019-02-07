d558fe90aeceaa786b19f170d907896ecb0d884e
James Clarke <cla16054@byui.edu>
2019-02-07 16:22:53 -0700

    upload tool page

--- web/toolData.php
+++ web/toolData.php
@@ -4,41 +4,34 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="navBar.css">
+    <link rel="stylesheet" type="text/css" href="toolCss.css">
     <title>Tool Database</title>
 </head>
 <body>
-    <form>
-        Part Name: <input type="text"><br>
-        Part Number: <input type="text"><br>
-        Manufacturer: <input type="text"><br>
-        Picture Name: <input type="text"><br>
-        Location Type: <input type="text"><br>
-        Location: <input type="text"><br>
+<div id="formSearch">
+    <form method="post" action="">
+        <input type="search" name="part_number" placeholder="Search Part Number"><br>
+        <input type="radio" name="location_type" value="1"> Show all owned by James<br>
+        <input type="radio" name="location_type" value="2" > Show all owned by Richard<br>
+        <input type="button" name="submit" value="Submit">
     </form>
-    
+</div>
+<div id="formInput">
+    <form method="post" action="">
+        <input type="text" name="part_name" placeholder="Part Name"><br>
+        <input type="text" name="part_number" placeholder="Part Number"><br>
+        <input type="text" name="manufacturer" placeholder="Manufacturer Name"><br>
+        <input type="text" name="picture_name" placeholder="Image Name (jpg)"><br>
+        <input type="radio" name="location_type" value="1"> Owned by James<br>
+        <input type="radio" name="location_type" value="2" > Owned by Richard<br>
+        <input type="text" name="location" placeholder="Location"><br>
+        <input type="button" name="submit" value="Submit">
+    </form>
+</div>
+
+<div id="dbOutput">
 <?php
-try
-{
-$dbUrl = getenv('DATABASE_URL');
-
-$dbOpts = parse_url($dbUrl);
-
-$dbHost = $dbOpts["host"];
-$dbPort = $dbOpts["port"];
-$dbUser = $dbOpts["user"];
-$dbPassword = $dbOpts["pass"];
-$dbName = ltrim($dbOpts["path"],'/');
-
-$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-}
-catch (PDOException $ex)
-{
-echo 'Error!: ' . $ex->getMessage();
-die();
-}
+include "./db_connect.php";
 
 foreach ($db->query('SELECT bushing_id, bushing_name, part_number, manufacturer, picture_name FROM bushings') as $row)
 {
@@ -68,7 +61,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo $rows[$bushing_name];
 ?>
 
-
+</div>
 
 
 </body>
