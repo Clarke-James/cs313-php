29c05f77c4d28291e307bdc6a415c71edef24000
James Clarke <cla16054@byui.edu>
2019-02-15 16:51:59 -0700

    new update page

--- web/toolUpdate.php
+++ web/toolUpdate.php
@@ -17,19 +17,21 @@ require "./db_connect.php";
 
     if (isset($_POST['part_number'])){
         $unSafe_partNumber = htmlspecialchars(strtoupper($_POST['part_number']));
+        //http://www.php.net/manual/en/function.pg-escape-string.php
+        $partNumber = pg_escape_string($unSafe_partNumber);
     }
 
     if (isset ($_POST['part_number'])){
 
-        //http://www.php.net/manual/en/function.pg-escape-string.php
-        $partNumber = pg_escape_string($unSafe_partNumber);
+
+
 
         foreach ($db->query("SELECT * FROM bushings AS b  JOIN location AS l ON b.bushing_id = l.bushing_id 
                 WHERE part_number = '$partNumber'")as $rows)
         {
             echo "Allowed Changes for part number: " . $rows['part_number'] . "<br>";
             echo "<form method='post' action='toolChanges.php'>";
-            echo "<input type='hidden' name='part_number' placeholder='" . $rows['part_number'] . "'>";
+            echo "<input type='hidden' name='part_number' placeholder='" . $rows['part_number'] . "'><br>";
             echo "<input type='text' name='part_name' placeholder='" . $rows['bushing_name'] . "'> Name<br>";
             echo "<input type='text' name='manufacturer' placeholder='" . $rows['manufacturer']. "'> Manufacturer<br>";
             echo "<input type='text' name='picture_name' placeholder='" . $rows['picture_name'] . "'> Image name<br>";
