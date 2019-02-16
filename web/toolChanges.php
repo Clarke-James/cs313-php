2911d561e3f83a988a1a719139c0f053424a4fec
James Clarke <cla16054@byui.edu>
2019-02-15 19:51:09 -0700

    new update page

--- web/toolChanges.php
+++ web/toolChanges.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Tool View</title>
-</head>
-<body>
 <?php
 require "./db_connect.php";
 
@@ -18,17 +10,11 @@ if (isset($_POST['location'])) {
 if (isset($_POST['bushing_id'])){
    $bushingId = ($_POST['bushing_id']);
 }
-//$stmt = $db->prepare("SELECT bushing_id FROM bushings WHERE part_number = '$partNumber'");
-//$stmt->execute(':bushing_id' => $bushing_id);
-//$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo "bushing Id is : " . var_dump($bushingId);
 $stmt = $db->prepare("UPDATE location SET location = '$location' WHERE bushing_id = $bushingId");
 $stmt->execute();
 
-//$newPage = "toolData.php";
-//header("Location: $newPage");
-//die();
+$newPage = "toolData.php";
+header("Location: $newPage");
+die();
 ?>
-</body>
-</html>
