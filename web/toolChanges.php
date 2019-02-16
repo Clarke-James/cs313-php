c2f9b38a2bf21d5f4b3278d764b658ce92b5f458
James Clarke <cla16054@byui.edu>
2019-02-15 18:22:59 -0700

    new update page

--- web/toolChanges.php
+++ web/toolChanges.php
@@ -7,13 +7,14 @@ if (isset($_POST['part_number'])){
 if (isset($_POST['location'])) {
     $location = htmlspecialchars(strtoupper($_POST['location']));
 }
-if (isset($_POST['bushing_id'])) {
-    $bushingId = htmlspecialchars($_POST['bushing_id']);
+if (isset($_POST['bushing_id'])){
+    $unSafe_bushingId = htmlspecialchars(strtoupper($_POST['bushing_id']));
+    //http://www.php.net/manual/en/function.pg-escape-string.php
+    $bushingId = pg_escape_string($unSafe_bushingId);
 }
 
 $stmt2 = $db->prepare("UPDATE location SET location = '$location' WHERE bushing_id = $bushingId");
 //$stmt2->bindValue(':bushingId', $bushingId, PDO::PARAM_INT);
-//$stmt2->bindValue(':locationType', $locType, PDO::PARAM_INT);
 //$stmt2->bindValue(':location', $location, PDO::PARAM_STR);
 $stmt2->execute();
 
