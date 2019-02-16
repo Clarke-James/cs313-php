dd0203b5250173b8e17eee731e5795608fc46534
James Clarke <cla16054@byui.edu>
2019-02-15 19:38:47 -0700

    new update page

--- web/toolChanges.php
+++ web/toolChanges.php
@@ -15,12 +15,12 @@ if (isset($_POST['part_number'])){
 if (isset($_POST['location'])) {
     $location = htmlspecialchars(strtoupper($_POST['location']));
 }
-//if (isset($_POST['bushing_id'])){
- //  $bushingId = ($_POST['bushing_id']);
-//}
-$stmt = $db->prepare("SELECT bushing_id FROM bushings WHERE part_number = '$partNumber'");
-$stmt->execute();
-$bushingId = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (isset($_POST['bushing_id'])){
+   $bushingId = ($_POST['bushing_id']);
+}
+//$stmt = $db->prepare("SELECT bushing_id FROM bushings WHERE part_number = '$partNumber'");
+//$stmt->execute(':bushing_id' => $bushing_id);
+//$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo "bushing Id is : " . var_dump($bushingId);
 $stmt = $db->prepare("UPDATE location SET location = '$location' WHERE bushing_id = $bushingId");
