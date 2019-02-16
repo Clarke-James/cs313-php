9f456bae09316102d40f163db89cfddb620c21fe
James Clarke <cla16054@byui.edu>
2019-02-15 17:00:14 -0700

    new update page

--- web/toolChanges.php
+++ web/toolChanges.php
@@ -24,15 +24,10 @@ if (isset($_POST['bushing_id'])) {
     $bushingId = htmlspecialchars($_POST['bushing_id']);
 }
 
-$stmt = $db->prepare("UPDATE bushings SET bushing_name = :bushing_name, manufacturer = :manufacturer, picture_name = :picture_name WHERE part_number = $partNumber");
-$stmt->bindValue(':bushing_name', $bName, PDO::PARAM_STR);
-$stmt->bindValue(':manufacturer', $manufacturer, PDO::PARAM_STR);
-$stmt->bindValue(':picture_name', $imgName, PDO::PARAM_STR);
+$stmt = $db->prepare("UPDATE bushings SET bushing_name = $bName, manufacturer = $manufacturer, picture_name = $imgName WHERE part_number = $partNumber");
 $stmt->execute();
 
-$stmt2 = $db->prepare("UPDATE location SET location_type = :location_type, location = :location WHERE bushing_id = $bushingId");
-$stmt2->bindValue(':location_type', $locType, PDO::PARAM_INT);
-$stmt2->bindValue(':location', $location, PDO::PARAM_STR);
+$stmt2 = $db->prepare("UPDATE location SET location_type = $locType, location = $location WHERE bushing_id = $bushingId");
 $stmt2->execute();
 
 $newPage = "toolData.php";
